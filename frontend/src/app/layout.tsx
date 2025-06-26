4f96b33924636d412175ca3d4118262652167068
gian <lgian584@gmail.com>
2025-06-26 13:51:07 -0300

    feat: implement PostContext and PostProvider for managing posts state

--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -1,5 +1,6 @@
 import Footer from "@/components/layout/Footer";
 import Header from "@/components/layout/Header";
+import { PostProvider } from "@/contexts/PostContext";
 import { UserProvider } from "@/contexts/UserContext";
 import type { Metadata } from "next";
 import { Roboto } from "next/font/google";
@@ -27,11 +28,13 @@ export default function RootLayout({
         className={`${roboto.variable} antialiased min-h-screen flex flex-col bg-gray-50`}
       >
         <UserProvider>
-          <Header />
-          <main className="flex-1 max-w-6xl mx-auto w-full px-4 sm:px-6 lg:px-8 py-8">
-            {children}
-          </main>
-          <Footer />
+          <PostProvider>
+            <Header />
+            <main className="flex-1 max-w-6xl mx-auto w-full px-4 sm:px-6 lg:px-8 py-8">
+              {children}
+            </main>
+            <Footer />
+          </PostProvider>
         </UserProvider>
       </body>
     </html>
